652c4bc4b976080a6f621432300f82945169f6d8
rob144 <rob144@users.noreply.github.com>
2015-01-25 22:11:49 +0000

    Added test page with smooth left and right transitions.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 Paint Survey
 App to help painting contractors estimate costs.
+
+cd paint-survey
+
+#To run
+<path-to-Python-SDK>/dev_appserver.py ./
+
+#To upload
+appcfg.py update ./
--- main.py
+++ main.py
@@ -2,20 +2,31 @@ import os
 import webapp2
 import jinja2
 
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+def include_file(filename):
+    return jinja2.Markup(loader.get_source(JINJA_ENV, filename)[0])
+
+loader=jinja2.FileSystemLoader(os.path.dirname(__file__))
+JINJA_ENV = jinja2.Environment(
+    loader=loader,
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+JINJA_ENV.globals['include_file'] = include_file
 
-class MainPage(webapp2.RequestHandler):
+class HomeHandler(webapp2.RequestHandler):
     def get(self):
         template_values = {
             'greeting': 'HELLO'
         }
 
-        template = JINJA_ENVIRONMENT.get_template('index.html')
+        template = JINJA_ENV.get_template('index.html')
         self.response.write(template.render(template_values))
 
+class TestHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENV.get_template('test.html')
+        self.response.write(template.render())
+
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', HomeHandler),
+    (r'/test', TestHandler)
 ], debug=True)
